fe972e42fdf8a9748b9e22788be62d08a39c47aa
jsamritha29 <jsamritha29@gmail.com>
2026-02-05 10:37:23 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,16 +1,60 @@
-import React , {useState} from "react";
+import React,{useState} from 'react'
 
-function App() {
-  const[Color,setColor]=useState("white")
- return (
-  <div>
-    <h1>Color-Picker</h1>
-    <button onClick={()=>setColor("red")}>Red</button>
-    <button onClick={()=>setColor("green")}>Green</button>
-    <button onClick={()=>setColor("blue")}>Blue</button>
-    <div className='bgchange' style={{backgroundColor:Color}}></div>
-  </div>
- )
+function TabComponent() {
+  const[activeTab,setactiveTab]=useState("Home")
+  return(
+    <div style={Styles.Container}>
+      <h2>Tab Component</h2>
+      <div style={Styles.tabContainer}>
+         <button style={activeTab === "Home" ? Styles.activeBtn: Styles.Btn}
+            onClick={()=>setactiveTab("Home")}>Home</button>
+         <button style={activeTab === "About" ? Styles.activeBtn: Styles.Btn}
+            onClick={()=>setactiveTab("About")}>About</button>
+         <button style={activeTab === "Contact" ? Styles.activeBtn: Styles.Btn}
+            onClick={()=>setactiveTab("Contact")}>Contact</button>
+
+      </div>
+      <div style={Styles.contentBox}>
+        {activeTab === "Home" && (
+          <p>This is Home page</p>
+        )}
+        {activeTab === "About" && (
+          <p>Hii About page i'm reactjs</p>
+        )}
+         {activeTab === "Contact" && (
+          <p>Contact us : </p>
+        )}
+         
+
+      </div>
+    </div>
+  )
+  
 }
+const Styles={
+  Container:{
+    minHeight:"200vh",
+    background:"lightpink",
+    textAlign:"center"
+  
+  },
+  tabContainer:{
+    padding:"20px"
 
-export default App
+  },
+  activeBtn:{
+    background:"green"
+  },
+  Btn:{
+    background:"white"
+  },
+  contentBox:{
+    height:"100px",
+    width:"200px",
+    border:"1px solid",
+    borderRadius:"10px",
+    marginLeft:"640px"
+
+  }
+}
+export default TabComponent
